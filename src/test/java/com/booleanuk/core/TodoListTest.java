5af6ca9923fb1aba1a503c20c1c5a1e5ec872016
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-09 14:10:39 +0100

    Add test for adding a task

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoListTest {
+//    @Test
+//    public void exampleTest() {
+//        String hello = "Hello";
+//        Assertions.assertEquals("Hello", hello);
+//        Assertions.assertNotEquals("Goodbye", hello);
+//    }
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void canAddTask() {
+        String name = "Study";
+        TodoList todo = new TodoList();
+        Assertions.assertTrue(todo.add(name));
     }
 }
