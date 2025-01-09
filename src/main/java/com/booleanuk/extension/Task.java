dae499a2c7e5918539abb7fb5e48f976d8407ca7
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-09 16:54:17 +0100

    Add class Task

---
+++ src/main/java/com/booleanuk/extension/Task.java
@@ -0,0 +1,38 @@
+package com.booleanuk.extension;
+
+public class Task {
+    private static int nextId = 0;
+    private int id;
+    private boolean completed;
+    private String name;
+
+    public Task(String name) {
+        completed = false;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
