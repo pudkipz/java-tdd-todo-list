08b9cb6475fa3cb87d1a3acce731c720c97e2c35
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-09 14:16:35 +0100

    Implement add() method

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,7 +1,17 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TodoList {
+    Map<String, Boolean> todo = new HashMap<>();
+    // <name, status>
+
     public boolean add(String name) {
+        if (this.todo.containsKey(name)) {
+            return false;
+        }
+        this.todo.put(name, false);
         return true;
     }
 }
